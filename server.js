46bc6993444b90c1c93cd39d6dcefe13965f88bd
StefanJevtic63 <105609432+StefanJevtic63@users.noreply.github.com>
2022-05-18 00:18:24 +0200

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const path = require('path');
-const app = require('./app');
-
-const server = http.createServer(app);
-server.listen(3000);
-server.once('listening', function(req, res, next){
-    console.log("Server je pokrenut na adresi htttp://localhost:3000");
-})

114c9767060dd32c4639105f7f5ab4fe4f1775f2
StefanJevtic63 <105609432+StefanJevtic63@users.noreply.github.com>
2022-05-18 00:24:23 +0200

    Create student.js

---
+++ controllers/student.js
@@ -0,0 +1,62 @@
+const studentModel = require('../models/student');
+
+function getStudentByUsername(req, res, next){
+    const username= req.body.username;
+    const password = req.body.password;
+
+    const student = studentModel.getStudentByUsername(username);
+    if(student === null){
+        res.render('error.ejs', {
+            message: 'Pogresan username!'
+        });
+        return;
+    }
+
+    const passwordsMatch = studentModel.doPasswordsMatch(student, password);
+
+    if(!passwordsMatch){
+        res.render('error.ejs', {
+            message: 'Pogresili ste lozinku, pokusajte ponovo'
+        });
+        return;
+    }
+
+    if(passwordsMatch){
+        res.render('student.ejs', {
+            title: username,
+            student: student
+        });
+    }
+    
+};
+
+function updateStudentInfo(req, res, next){
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+    const major = req.body.major;
+
+    const student = studentModel.getStudentByUsername(username);
+    if(student === null){
+        res.render('error.ejs', {
+            message: 'Pogresan username!'
+        });
+        return;
+    }
+
+    student.password = password;
+    student.name = name;
+    student.major = major;
+
+    studentModel.updateStudentInfo(student);
+
+    res.render('student.ejs', {
+        title: username,
+        student: student
+    });
+}
+
+module.exports = {
+    getStudentByUsername,
+    updateStudentInfo
+}

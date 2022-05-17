fa9dfe3a3357a131e54783f6367b63d4438fe1a1
StefanJevtic63 <105609432+StefanJevtic63@users.noreply.github.com>
2022-05-18 00:22:53 +0200

    Create student.js

---
+++ routes/student.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const studentController = require('../controllers/student');
+
+const router = express.Router();
+
+router.get('/student', function(req, res, next){
+    const data = req.query;
+    console.log("Username: ", data.username);
+    console.log("Password: ", data.password);
+    res.sendFile(path.join(__dirname, "..", "public", "student.html"));
+});
+
+//Objekti za rutiranje treba da kazu koji kontroleri treba da obrade koje zahteve
+router.post('/student', studentController.getStudentByUsername);
+router.post('/student/update', studentController.updateStudentInfo);
+ 
+module.exports = router;
